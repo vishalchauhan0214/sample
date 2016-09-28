c30086cef629a31879a7d3c1d1d530b6c41506ca
vishal Singh Chauhan <vishals@enlume.com>
2016-09-28 16:50:17 +0530

    sdf

---
+++ TestOld.java
@@ -0,0 +1,20 @@
+package com.mkyong.common.controller;
+
+
+public class TestOld {
+
+
+    public static void main (String[] args) throws Exception {
+    	
+    	String temp = "cn=regcsr,ou=member,dc=ghc,dc=org^cn=authcsr,ou=member,dc=ghc,dc=org^cn=mychsdartcsr,ou=member,dc=ghc,dc=org";
+    	if(temp.indexOf("mychartcsr")!=-1)
+    	{
+    	   System.out.println("there is 'b' in temp string");
+    	}
+    	else
+    	{
+    	   System.out.println("there is no 'b' in temp string");
+    	}
+
+    }
+}
